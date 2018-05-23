8de253118c63cbc6d0603c7aab115a2c664cd382
Michal <Michal.stachnik@my.liu.edu>
2018-05-23 00:24:41 -0400

    getting ready for deployment

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const socketIO = require('socket.io');
 const express = require('express');
 
 const app = express();
-// app.use(express.static(__dirname));
+const port = process.env.PORT || 8000;
 
 onRequest = (req, res) => {
-
   fs.readFile('./index.html', null, (err, data) => {
     if(err) {
       res.writeHead(404);
@@ -20,36 +19,18 @@ onRequest = (req, res) => {
     }
     res.end();
   });
-  // res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // res.write('here');
-  // res.end();
 };
 
-// onRequest = (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-//   myReadStream.pipe(res);
-// }
-
-let server = http.createServer(onRequest).listen(8000, () => console.log('listening on 8000'));
-
-// let server = http.createServer(app).listen(8000);
-
-// let server = app.listen(8000, () => {
-//   console.log('listening on 8000');
-// });
+let server = 
+  http
+    .createServer(onRequest)
+    .listen(port, () => console.log('listening on 8000'));
+    
 let io = socketIO(server);
-
 
 io.on('connection', (socket) => {
   console.log('CONNECTED', socket.id);
-
-  // socket.on('data', (data) => {
-  //   io.sockets.emit('received', data)
-  // });
-
   setInterval(() => {
-    console.log('sending', Date.now());
     io.sockets.emit('received', {message: os.freemem() / os.totalmem()});
   }, 1000); 
 
